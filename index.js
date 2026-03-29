e878381511e463f31a8e0d486977ba285f96cd4a
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-03-29 18:50:56 +0600

    server deployment in vercel intitated

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+
         const transactionsCollection = client.db("FineEaseDB").collection("transactions")
 
         app.get('/my-transactions/:email', async (req, res) => {
@@ -65,12 +64,7 @@ async function run() {
             const result = await transactionsCollection.deleteOne(query);
             res.send(result)
         })
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
